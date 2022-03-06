f8fb1de1ac7def7a6ff4fa632a81ad16f824ceb7
david okesanya <david.okesanya9@gmail.com>
2022-03-06 22:57:10 +0100

    a Procfile

--- src/server/index.js
+++ src/server/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const Promise = require('es6-promise').Promise;
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8081;
 
 const WEATHER_A_BIT_API_KEY = process.env.WEATHER_A_BIT_API_KEY;
 const USER_Name = process.env.USER_Name;
@@ -87,8 +87,8 @@ app.post('/getTrip', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`listening at ${port}`);
+app.listen(PORT, () => {
+  console.log(`listening at ${PORT}`);
 });
 
 module.exports = app;
